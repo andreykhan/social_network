4f1750c2ac3f6635e0fc2568bf02367810409d71
Andrey Khan <andreykhan113@gmail.com>
2023-05-10 14:07:34 +0600

    finished main page and pages of groups

--- social_network/posts/views.py
+++ social_network/posts/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
-    return HttpResponse('main page')
+    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts
+    }
+    return render(request, template, context)
 
 
 def posts(request, slug):
-    return HttpResponse('posts page')
+    template = 'posts/group_list.html'
+    group  = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts
+    }
+    return render(request, template, context)
