64e75b38a33bcd7f6da46fd40f9ade6586cee9d5
Andrey Khan <andreykhan113@gmail.com>
2023-05-30 11:31:41 +0600

    project isorted

--- social_network/posts/tests/test_forms.py
+++ social_network/posts/tests/test_forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
 
-from posts.models import Group, Post, Comment
+from posts.models import Comment, Group, Post
 
 User = get_user_model()
 
--- social_network/posts/views.py
+++ social_network/posts/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.cache import cache_page
+from django.views.decorators.csrf import csrf_exempt
 
 from .forms import CreateComment, CreatePost
-from .models import Comment, Group, Post, User, Follow
+from .models import Comment, Follow, Group, Post, User
 
 
 #@cache_page(20*1, key_prefix='index_page')
