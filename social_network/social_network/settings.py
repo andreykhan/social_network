70bbc72b6d1bed785be2063a610ace4c54801304
Andrey Khan <andreykhan113@gmail.com>
2023-07-03 13:12:49 +0600

    configured dockerfile, docker-compose

--- social_network/social_network/settings.py
+++ social_network/social_network/settings.py
@@ -30,7 +30,8 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     '[::1]',
-    'testserver'
+    'testserver',
+    'web'
 ]
 
 
@@ -140,10 +141,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'posts:main_page'
